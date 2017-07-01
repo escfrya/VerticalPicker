9f66c0d69c85fbd78d84c358654a8862f306f66c
Igor Skovorodkin <escfrya@mail.ru>
2017-07-01 23:48:31 +0300

    unit tests for OffsetCalculator

--- VerticalPickerTests/VerticalPickerTests.swift
+++ VerticalPickerTests/VerticalPickerTests.swift
@@ -21,7 +21,47 @@ class VerticalPickerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        
+    // MARK: - top offset by value
+    
+    func testTopOffsetForZeroValue() {
+        XCTAssertEqual(OffsetCalculator.topOffsetBy(value: 0, minValue: 0, maxValue: 1, height: 100, reversed: false), 100)
+    }
+    
+    func testTopOffsetForMaxValue() {
+        XCTAssertEqual(OffsetCalculator.topOffsetBy(value: 1, minValue: 0, maxValue: 1, height: 100, reversed: false), 0)
+    }
+    
+    func testTopOffsetForAverageValue() {
+        XCTAssertEqual(OffsetCalculator.topOffsetBy(value: 0.7, minValue: 0, maxValue: 1, height: 100, reversed: false), 30)
+    }
+    
+    func testTopOffsetForValueLessMin() {
+        XCTAssertEqual(OffsetCalculator.topOffsetBy(value: -1, minValue: 0, maxValue: 1, height: 100, reversed: false), 100)
+    }
+    
+    func testTopOffsetForValueGreaterMax() {
+        XCTAssertEqual(OffsetCalculator.topOffsetBy(value: 2, minValue: 0, maxValue: 1, height: 100, reversed: false), 0)
+    }
+    
+    // MARK: - value by top offset
+    
+    func testValueForZeroTopOffset() {
+        XCTAssertEqual(OffsetCalculator.valueBy(topOffset: 0, minValue: 0, maxValue: 1, height: 100, reversed: false), 1)
+    }
+    
+    func testValueForMaxTopOffset() {
+        XCTAssertEqual(OffsetCalculator.valueBy(topOffset: 100, minValue: 0, maxValue: 1, height: 100, reversed: false), 0)
+    }
+    
+    func testValueForAverageTopOffset() {
+        XCTAssertEqual(OffsetCalculator.valueBy(topOffset: 30, minValue: 0, maxValue: 1, height: 100, reversed: false), 0.7)
+    }
+    
+    func testValueForTopOffsetLessMin() {
+        XCTAssertEqual(OffsetCalculator.valueBy(topOffset: -50, minValue: 0, maxValue: 1, height: 100, reversed: false), 1)
+    }
+    
+    func testValueForTopOffsetGreaterMax() {
+        XCTAssertEqual(OffsetCalculator.valueBy(topOffset: 150, minValue: 0, maxValue: 1, height: 100, reversed: false), 0)
     }
 }
